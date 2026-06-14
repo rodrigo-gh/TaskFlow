9f24c7730212cb57a52a48f789e46caa9944d28f
Rodrigo Knop <rodrigo_knop@outlook.com>
2026-06-14 15:26:00 -0300

    feat(api): add task request validation

--- TaskFlow.api/DTOs/CreateTaskRequest.cs
+++ TaskFlow.api/DTOs/CreateTaskRequest.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TaskFlow.Api.DTOs;
 
 public class CreateTasksRequest
 {
+    [Required(ErrorMessage = "Title is required.")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "Title must be between 3 and 100 characters.")]
     public string Title { get; set; } = string.Empty;
+
+    [StringLength(500, ErrorMessage = "Description must have at most 500 characters.")]
     public string? Description { get; set; }
 }
--- TaskFlow.api/DTOs/UpdateTaskRequest.cs
+++ TaskFlow.api/DTOs/UpdateTaskRequest.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TaskFlow.Api.DTOs;
 
 public class UpdateTaskRequest
 {
+    [Required(ErrorMessage = "Title is required.")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "Title must be between 3 an 100 characters.")]
     public string Title { get; set; } = string.Empty;
+
+    [StringLength(500, ErrorMessage = "Description must have at most 500 characters.")]
     public string? Description { get; set; }
+
     public bool IsCompleted { get; set; }
 }
