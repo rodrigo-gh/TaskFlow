eb272e5d119907c802f857b330c13fa2e8b95be1
Rodrigo Knop <rodrigo_knop@outlook.com>
2026-06-16 15:58:23 -0300

    chore(app): configure API base URL with dart define

--- taskflow_app/lib/app/core/constants/api_constants.dart
+++ taskflow_app/lib/app/core/constants/api_constants.dart
@@ -1,5 +1,8 @@
 class ApiConstants {
-  // static const String baseUrl = 'http://localhost:5099';
-  static const String baseUrl = 'http://192.168.18.84:5099';
-  
+  static const String baseUrl = String.fromEnvironment(
+    'API_BASE_URL',
+    defaultValue: 'http://localhost:5099',
+  );
 }
+
+// static const String baseUrl = 'http://192.168.18.84:5099';
